91dae17278373099d67081fe6719ca2ecb8bb989
Bobby O'Brien <contact@bobbyobrien.com>
2024-04-28 23:28:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
 import random
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from PIL import ImageGrab
 
 # Screenshot random intervals
-min_interval = 19
-max_interval = 23
+min_interval = 19 * 60
+max_interval = 23 * 60
 
 
 def get_random_interval():
@@ -38,10 +38,10 @@ def take_screenshot():
 
         # Get a new random interval
         next_interval = get_random_interval()
-        print(f"Next screenshot in {next_interval} minutes")
+        print(f"Next screenshot at {(datetime.now() + timedelta(seconds=next_interval)).strftime('%H:%M:%S')}")
 
         # Wait for the next interval
-        time.sleep(next_interval * 60)
+        time.sleep(next_interval)
 
     except Exception as e:
         print(f"Error capturing screenshot: {e}")
